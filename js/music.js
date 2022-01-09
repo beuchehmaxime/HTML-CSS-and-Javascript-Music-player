031b104e549d2d9631fc73b4f174f8e25d859960
beuchehmaxime <maximebeucheh86@gmail.com>
2022-01-09 08:20:48 +0100

    First commit to this project

---
+++ js/music.js
@@ -0,0 +1,27 @@
+let song_list =  [
+    {
+        name : 'Jovi ft Tata',
+        title : '237%',
+        src : 'music1.mp3',
+        img : '4.jpg',
+    },
+   {
+        name : 'Jovi ft Ishaku',
+        title : 'Pressure',
+        src : 'music2.mp3',
+        img : '4.jpg',
+    },
+   {
+        name : 'Jovi ft Mamiso',
+        title : 'Full Effect',
+        src : 'music3.mp3',
+        img : '4.jpg',
+    },
+    {
+        name : 'Jovi',
+        title : 'Ipso Factor',
+        src : 'music4.mp3',
+        img : '4.jpg',
+    },
+   
+]
